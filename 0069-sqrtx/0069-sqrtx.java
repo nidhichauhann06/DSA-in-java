26402c7fad7399b6bf1d27ac79538516063fb229
Nidhi Chauhan <nidhichauhann06@gmail.com>
2026-08-01 20:02:58 +0530

    Time: 1 ms (97.48%), Space: 42.7 MB (43.43%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        int low = 1;
+        int high = x;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            long square = (long)mid*mid;
+            if(square == x){
+                return mid;
+            }else if(square > x){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return high;
+    }
+}
